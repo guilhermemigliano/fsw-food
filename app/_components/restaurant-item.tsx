2755b504d7c7fec14b39bf5348e95a142c421202
Guilherme Migliano <44805868+guilhermemigliano@users.noreply.github.com>
2024-05-01 19:38:07 -0300

    feat: add restaurant page

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -3,6 +3,7 @@ import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { formatCurrency } from "../_helpers/price";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface RestaurantItemProps {
   restaurant: Restaurant;
@@ -10,50 +11,55 @@ interface RestaurantItemProps {
 
 const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
   return (
-    <div className="min-w-[266px] max-w-[266px] space-y-3">
-      <div className="w-full h-[136px] relative">
-        <Image
-          src={restaurant.imageUrl}
-          fill
-          className="object-cover rounded-lg"
-          alt="teste"
-        />
+    <Link
+      className="min-w-[266px] max-w-[266px]"
+      href={`/restaurants/${restaurant.id}`}
+    >
+      <div className="w-full  space-y-3">
+        <div className="w-full h-[136px] relative">
+          <Image
+            src={restaurant.imageUrl}
+            fill
+            className="object-cover rounded-lg"
+            alt="teste"
+          />
 
-        <div className="absolute top-2 left-2 gap-[2px] bg-primary py-[2px] px-2 rounded-full bg-white flex items-center">
-          <StarIcon size={12} className="fill-yellow-500 text-yellow-400" />
-          <span className="font-semibold text-xs">5</span>
-        </div>
+          <div className="absolute top-2 left-2 gap-[2px] bg-primary py-[2px] px-2 rounded-full bg-white flex items-center">
+            <StarIcon size={12} className="fill-yellow-500 text-yellow-400" />
+            <span className="font-semibold text-xs">5</span>
+          </div>
 
-        <Button
-          size="icon"
-          className="absolute right-2 top-2 bg-gray-700 rounded-full h-7 w-7"
-        >
-          <HeartIcon size={12} className="fill-white" />
-        </Button>
-      </div>
-      <div>
-        <h3 className="text-sm font-semibold">{restaurant.name}</h3>
-        {/* INFORMAÇÕES DA ENTREGA */}
-        <div className="flex gap-3">
-          {/* CUSTO DE ENTREGA */}
-          <div className="flex gap-1 items-center">
-            <BikeIcon size={14} className="text-primary" />
-            <span className="text-xs text-muted-foreground">
-              {Number(restaurant.deliveryFee) === 0
-                ? "Entrega grátis"
-                : formatCurrency(Number(restaurant.deliveryFee))}
-            </span>
-          </div>
-          {/* TEMPO DE ENTREGA */}
-          <div className="flex gap-1 items-center">
-            <TimerIcon size={14} className="text-primary" />
-            <span className="text-xs text-muted-foreground">
-              {restaurant.deliveryTimeMinutes} min
-            </span>
+          <Button
+            size="icon"
+            className="absolute right-2 top-2 bg-gray-700 rounded-full h-7 w-7"
+          >
+            <HeartIcon size={12} className="fill-white" />
+          </Button>
+        </div>
+        <div>
+          <h3 className="text-sm font-semibold">{restaurant.name}</h3>
+          {/* INFORMAÇÕES DA ENTREGA */}
+          <div className="flex gap-3">
+            {/* CUSTO DE ENTREGA */}
+            <div className="flex gap-1 items-center">
+              <BikeIcon size={14} className="text-primary" />
+              <span className="text-xs text-muted-foreground">
+                {Number(restaurant.deliveryFee) === 0
+                  ? "Entrega grátis"
+                  : formatCurrency(Number(restaurant.deliveryFee))}
+              </span>
+            </div>
+            {/* TEMPO DE ENTREGA */}
+            <div className="flex gap-1 items-center">
+              <TimerIcon size={14} className="text-primary" />
+              <span className="text-xs text-muted-foreground">
+                {restaurant.deliveryTimeMinutes} min
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
