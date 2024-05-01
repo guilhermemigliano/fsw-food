75885d294b387f4064081193d08ee2f5fda41c2b
Guilherme Migliano <44805868+guilhermemigliano@users.noreply.github.com>
2024-05-01 20:17:39 -0300

    feat: add recommended products page

--- app/page.tsx
+++ app/page.tsx
@@ -40,13 +40,15 @@ export default async function Home() {
       <div className="pt-6 space-y-4">
         <div className="px-5 flex justify-between items-center">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
-          <Button
-            variant={"ghost"}
-            className="text-primary p-0 hover:bg-transparent h-fit"
-          >
-            Ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href={"/products/recommended"}>
+            <Button
+              variant={"ghost"}
+              className="text-primary p-0 hover:bg-transparent h-fit"
+            >
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <ProductList products={products} />
       </div>
